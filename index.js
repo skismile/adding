f9ea2634f2b2f55c9d72c09e6ef2c8568b1308af
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-17 20:13:20 +0530

    palindrome featcher

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-let a=1
-let b=2
-let sum=a+b
-console.log(sum);
+// let a=1
+// let b=2
+// let sum=a+b
+// console.log(sum);
+
+// palindrome
+// let str="abba"
+// let n=str.length //4
+
+// let ulta=""
+// // console.log(n);
+// for(let i=n-1;i>=0;i--)
+// {
+// ulta=ulta+str[i]
+// // console.log(str[i])
+
+// }
+
+// console.log(str==ulta);
+
+
+function checkPalindrome(str){
+    let n=str.length
+    let left = 0;
+    let right = n - 1;
+    
+    while (left < right) {
+      if (str[left] == str[right]) {
+        left++;
+        right--;
+      } else {
+        return "no this is not"
+      }
+    }
+return "yes its palindrome"  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let str = "aaabaaa";
+let n = str.length;
+let ans=checkPalindrome(str)
+console.log(ans)
+
+
+let ans2=checkPalindrome("soyel")
+console.log(ans2);
+ //4
+
